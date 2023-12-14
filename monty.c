11b2b21aaae2e01fffa65cb61b23d5c25ef5d325
Bayelaye313 <bayelaye0510@gmail.com>
2023-12-14 14:09:52 +0000

    ff

--- monty.c
+++ monty.c
@@ -14,10 +14,34 @@ arg_t arg = {NULL, NULL, NULL, 0};
 */
 int main(int argc, char *argv[])
 {
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
+	unsigned int linenum = 0;
 
 	validate_arguments(argc, argv);
-	read_file(argv[1], &stack);
+	file = fopen(argv[1], "r");
+	arg.script = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		arg.content = content;
+		linenum++;
+		if (read_line > 0)
+		{
+			run(content, &stack, linenum, file);
+		}
+		free(content);
+	}
 	free_stack(stack);
+	fclose(file);
 return (0);
 }
--- run_instructs.c
+++ run_instructs.c
@@ -24,101 +24,60 @@ void init_args(void)
 	arg.content = NULL;
 	arg.linenum = 0;
 }
-/**
- * parse_line - parses a line for an opcode and arguments
- * @line: the line to be parsed
- *
- * Return: returns the opcode or null on failure
- */
-char *parse_line(char *line)
-{
-	char *op_code;
 
-	op_code = strtok(line, "\n ");
-	if (op_code == NULL)
-		return (NULL);
-	return (op_code);
-}
 /**
- * read_file - reads a bytecode file and runs commands
- * @filename: pathname to file
- * @top: pointer to the top of the stack
- *
- */
-void read_file(char *filename, stack_t **top)
+* run - function that run_instructionss the opcode
+* @top: head stack linked list
+* @linenum: line count
+* @script: pointer to monty file stream
+* @content: line content
+*
+* Return: nothing
+*/
+int run(char *content, stack_t **top, unsigned int linenum, FILE *script);
+int run(char *content, stack_t **top, unsigned int linenum, FILE *script)
 {
-	char *buffer = NULL;
-	char *line;
-	size_t i = 0;
-	int linenum = 1;
-	instruct_func s;
-	int check;
-	int read;
-	FILE *file = fopen(filename, "r");
+	instruction_t opst[] = {
+				{"push", _push},
+				{"pall", _pall},
+				{"pint", _pint},
+				{"pop", _pop},
+				{"swap", _swap},
+				{"add", _add},
+				{"nop", _nop},
+				{"sub", _sub},
+				{"div", _div},
+				{"mul", _mul},
+				{"mod", _mod},
+				{"pchar", _pchar},
+				{"pstr", _pstr},
+				{"rotl", _rotl},
+				{"rotr", _rotr},
+				{"queue", _queue},
+				{"stack", _stack},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
 
-	if (file == NULL)
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	arg.tok = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
 	{
-		printf("Error: Can't open file %s\n", filename);
-		exit(EXIT_FAILURE);
-	}
-	while ((read = getline(&buffer, &i, file)) != -1)
-	{
-		line = parse_line(buffer);
-		if (line == NULL || line[0] == '#')
-		{
-			linenum++;
-			continue;
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(top, linenum);
+			return (0);
 		}
-		s = get_op_func(line);
-		if (s == NULL)
-		{
-			printf("L%d: unknown instruction %s\n", linenum, line);
-			exit(EXIT_FAILURE);
-		}
-		s(top, linenum);
-		linenum++;
-	}
-	free(buffer);
-	check = fclose(file);
-	if (check == -1)
-		exit(-1);
-}
-/**
- * get_op_func -  checks opcode and returns the correct function
- * @str: the opcode
- *
- * Return: returns a function, or NULL on failure
- */
-instruct_func get_op_func(char *str)
-{
-	int i;
-
-	instruction_t instruct[] = {
-		{"push", _push},
-		{"pall", _pall},
-		{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"pchar", _pchar},
-		{"add", _add},
-		{"sub", _sub},
-		{"mul", _mul},
-		{"div", _div},
-		{"mod", _mod},
-		{"nop", _nop},
-		{"rotl", _rotl},
-		{"rotr", _rotr},
-		{"pstr", _pstr},
-		{"stack", _stack},
-		{"queue", _queue},
-		{NULL, NULL},
-	};
-
-	i = 0;
-	while (instruct[i].f != NULL && strcmp(instruct[i].opcode, str) != 0)
-	{
 		i++;
 	}
-
-	return (instruct[i].f);
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", linenum, op);
+		fclose(script);
+		free(content);
+		free_stack(*top);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
 }
