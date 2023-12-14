2dcc313b2de8dd779749e89f3da5a7d6763c8f12
Bayelaye313 <bayelaye0510@gmail.com>
2023-12-14 12:55:50 +0000

    tt

--- exec.c
+++ exec.c
@@ -16,90 +16,58 @@ void validate_arguments(int argc, char *argv[])
 }
 
 /**
- * init_args - Initializes the global argument structure.
- */
-void init_args(void)
-{
-	arg.tok = NULL;
-	arg.script = NULL;
-	arg.content = NULL;
-	arg.linenum = 0;
-}
-
-/**
- * get_script - Opens and sets the script file.
- * @filename: Name of the file to open.
- */
-void get_script(char *filename)
-{
-	FILE *file = fopen(filename, "r");
-
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", filename);
-		exit(EXIT_FAILURE);
-	}
-
-	arg.script = file;
-}
-/**
- * free_arguments - Frees memory allocated for arguments.
- */
-void free_arguments(void)
-{
-	free(arg.tok);
-	free(arg.content);
-}
-
-
-/**
- * get_instruction - Executes the appropriate opcode function.
- * @op: The opcode to execute.
- * @top: Head of the stack linked list.
- * @linenum: Line number.
- */
-void get_instruction(char *op, stack_t **top, unsigned int linenum);
-void get_instruction(char *op, stack_t **top, unsigned int linenum)
+* execute - function that executes the opcode
+* @top: head stack linked list
+* @linenum: line count
+* @script: pointer to monty file stream
+* @content: line content
+*
+* Return: nothing
+*/
+int execute(char *content, stack_t **top, unsigned int linenum, FILE *script);
+int execute(char *content, stack_t **top, unsigned int linenum, FILE *script)
 {
 	instruction_t opst[] = {
-		{"push", _push},
-		{"pall", _pall},
-		{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"add", _add},
-		{"nop", _nop},
-		{"sub", _sub},
-		{"div", _div},
-		{"mul", _mul},
-		{"mod", _mod},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"rotl", _rotl},
-		{"rotr", _rotr},
-		{"queue", _queue},
-		{"stack", _stack},
-		{NULL, NULL}
-	};
-
+				{"push", _push},
+				{"pall", _pall},
+				{"pint", _pint},
+				{"pop", _pop},
+				{"swap", _swap},
+				{"add", _add},
+				{"nop", _nop},
+				{"sub", _sub},
+				{"div", _div},
+				{"mul", _mul},
+				{"mod", _mod},
+				{"pchar", _pchar},
+				{"pstr", _pstr},
+				{"rotl", _rotl},
+				{"rotr", _rotr},
+				{"queue", _queue},
+				{"stack", _stack},
+				{NULL, NULL}
+				};
 	unsigned int i = 0;
+	char *op;
 
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	arg.tok = strtok(NULL, " \n\t");
 	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
-		{
-			opst[i].f(top, linenum);
-			return;
+		{	opst[i].f(top, linenum);
+			return (0);
 		}
 		i++;
 	}
-
 	if (op && opst[i].opcode == NULL)
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", linenum, op);
-		fclose(arg.script);
-		free_arguments();
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", linenum, op);
+		fclose(script);
+		free(content);
 		free_stack(*top);
 		exit(EXIT_FAILURE);
 	}
+	return (1);
 }
