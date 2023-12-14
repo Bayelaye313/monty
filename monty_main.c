de8f94eb41cd83b54589a122f713355a1e9df932
Bayelaye313 <bayelaye0510@gmail.com>
2023-12-14 13:27:33 +0000

    tt

--- monty_main.c
+++ monty_main.c
@@ -1,48 +1,46 @@
 #include "monty.h"
 #include <stdio.h>
+#define _GNU_SOURCE
 #include <stdlib.h>
 
 arg_t arg = {NULL, NULL, NULL, 0};
 
 /**
- * main - function for monty code interpreter
- * @argc: argument count
- * @argv: argument value
- *
- * Return: 0 on success
- */
+* main - function for monty code interpreter
+* @argc: argument count
+* @argv: argument value
+*
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
 	char *content;
+	FILE *file;
 	size_t size = 0;
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int linenum = 0;
-
 	validate_arguments(argc, argv);
-	init_args();
-	get_script(argv[1]);
-
-	if (arg.script == NULL)
+	file = fopen(argv[1], "r");
+	arg.script = file;
+	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, arg.script);
+		read_line = getline(&content, &size, file);
 		arg.content = content;
 		linenum++;
 		if (read_line > 0)
 		{
-			execute(content, &stack, linenum, arg.script);
+			execute(content, &stack, linenum, file);
 		}
 		free(content);
 	}
-
 	free_stack(stack);
-	fclose(arg.script);
-	return (0);
+	fclose(file);
+return (0);
 }
