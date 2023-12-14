1b7f7543c6527f55e97ffb1ebd52a67117559387
Bayelaye313 <bayelaye0510@gmail.com>
2023-12-14 18:05:08 +0000

    tt

--- lists.c
+++ lists.c
@@ -1,70 +1,60 @@
 #include "monty.h"
 
 /**
-* addnode - function that adds node to the top stack
-* @top: top of the stack
-* @n: new value
-*
-* Return: nothing
-*/
+ * addnode - adds a new node to the top of the stack
+ * @top: double pointer to the top of the stack
+ * @n: new value to be added
+ */
 void addnode(stack_t **top, int n)
 {
-	stack_t *new_node, *temp;
-
-	temp = *top;
-	new_node = malloc(sizeof(stack_t));
+	stack_t *new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
-	{ printf("Error\n");
-		exit(0); }
-	if (temp)
-		temp->prev = new_node;
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
 	new_node->n = n;
-	new_node->next = *top;
 	new_node->prev = NULL;
+	new_node->next = *top;
+
+	if (*top != NULL)
+		(*top)->prev = new_node;
+
 	*top = new_node;
 }
 
 /**
-* free_stack - function that frees a doubly linked list
-* @top: top pointer to the stack
-*
-* Return: nothing
-*/
+ * free_stack - frees a doubly linked list
+ * @top: pointer to the top of the stack
+ */
 void free_stack(stack_t *top)
 {
-	stack_t *temp;
-
-	temp = top;
-	while (top)
+	while (top != NULL)
 	{
-		temp = top->next;
-		free(top);
-		top = temp;
+		stack_t *temp = top;
+		top = top->next;
+		free(temp);
 	}
 }
 
 /**
-* _stack - function that prints the top
-* @top: top of stack
-* @linenum: line count
-*
-* Return: nothing
-*/
+ * _stack - sets the data structure to stack (LIFO)
+ * @top: double pointer to the top of the stack
+ * @linenum: line number of the opcode in the Monty file
+ */
 void _stack(stack_t **top, unsigned int linenum)
 {
 	(void)top;
 	(void)linenum;
 	arg.linenum = 0;
 }
-#include "monty.h"
 
 /**
-* _queue - function that prints the top of the queue
-* @top: top of queue
-* @linenum: line count
-*
-* Return: nothing
-*/
+ * _queue - sets the data structure to queue (FIFO)
+ * @top: double pointer to the top of the stack
+ * @linenum: line number of the opcode in the Monty file
+ */
 void _queue(stack_t **top, unsigned int linenum)
 {
 	(void)top;
@@ -73,36 +63,33 @@ void _queue(stack_t **top, unsigned int linenum)
 }
 
 /**
-* addqueue - function that add node to the tail the queue
-* @n: new value
-* @top: top of the queue
-*
-* Return: nothing
-*/
+ * addqueue - adds a new node to the end of the queue
+ * @top: double pointer to the top of the queue
+ * @n: new value to be added
+ */
 void addqueue(stack_t **top, int n)
 {
-	stack_t *new_node, *temp;
-
-	temp = *top;
-	new_node = malloc(sizeof(stack_t));
+	stack_t *new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
-		printf("Error\n");
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
+
 	new_node->n = n;
 	new_node->next = NULL;
-	if (temp)
-	{
-		while (temp->next)
-			temp = temp->next;
-	}
-	if (!temp)
+
+	if (*top == NULL)
 	{
 		*top = new_node;
 		new_node->prev = NULL;
 	}
 	else
 	{
+		stack_t *temp = *top;
+		while (temp->next != NULL)
+			temp = temp->next;
+
 		temp->next = new_node;
 		new_node->prev = temp;
 	}
--- oprot.c
+++ oprot.c
@@ -1,55 +1,55 @@
 #include "monty.h"
 
 /**
-* _rotl- function that rotates the stack to the top
-* @top: top of the stack
-* @linenum: line count
-*
-* Return: nothing
-*/
-void _rotl(stack_t **top,  __attribute__((unused)) unsigned int linenum)
+ * _rotl - rotates the stack to the top
+ * @top: double pointer to the top of the stack
+ * @linenum: line number of the opcode in the Monty file
+ *
+ * Description: The top element of the stack becomes the last one,
+ * and the second top element of the stack becomes the first one.
+ */
+void _rotl(stack_t **top, __attribute__((unused)) unsigned int linenum)
 {
-	stack_t *tmp = *top, *aux;
+	if (*top != NULL && (*top)->next != NULL)
+	{
+		stack_t *new_top = (*top)->next;
+		stack_t *current = *top;
 
-	if (*top == NULL || (*top)->next == NULL)
-	{
-		return;
+		while (current->next != NULL)
+			current = current->next;
+
+		current->next = *top;
+		(*top)->prev = current;
+		(*top)->next = NULL;
+		new_top->prev = NULL;
+		*top = new_top;
 	}
-	aux = (*top)->next;
-	aux->prev = NULL;
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-	}
-	tmp->next = *top;
-	(*top)->next = NULL;
-	(*top)->prev = tmp;
-	(*top) = aux;
 }
 
 /**
-* _rotr - function that rotates the stack to the bottom
-* @top: stack top of the stack
-* @linenum: line count
-*
-* Return: nothing
-*/
+ * _rotr - rotates the stack to the bottom
+ * @top: double pointer to the top of the stack
+ * @linenum: line number of the opcode in the Monty file
+ *
+ * Description: The last elem of the stack becomes the top elem of the stack.
+ */
 void _rotr(stack_t **top, __attribute__((unused)) unsigned int linenum)
 {
-	stack_t *copy;
+	if (*top != NULL && (*top)->next != NULL)
+	{
+		stack_t *current = *top;
+		stack_t *new_top = NULL;
 
-	copy = *top;
-	if (*top == NULL || (*top)->next == NULL)
-	{
-		return;
+		while (current->next != NULL)
+		{
+			new_top = current;
+			current = current->next;
+		}
+
+		(*top)->prev = current;
+		current->next = *top;
+		current->prev->next = NULL;
+		current->prev = NULL;
+		*top = current;
 	}
-	while (copy->next)
-	{
-		copy = copy->next;
-	}
-	copy->next = *top;
-	copy->prev->next = NULL;
-	copy->prev = NULL;
-	(*top)->prev = copy;
-	(*top) = copy;
 }
