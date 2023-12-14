848efe351f43a93ff7a68be3b2ca8da13af45afb
Bayelaye313 <bayelaye0510@gmail.com>
2023-12-14 12:48:14 +0000

    resr

--- monty_main.c
+++ monty_main.c
@@ -1,60 +1,51 @@
 #include "monty.h"
-#include <stdlib.h>
 #include <stdio.h>
 #define _GNU_SOURCE
-/**
- * parse_line - Parses a line for an opcode and arguments.
- * @line: The line to be parsed.
- *
- * Return: Returns the opcode or NULL on failure.
- */
-char *parse_line(char *line)
-{
-	char *op_code;
-
-	op_code = strtok(line, "\n ");
-	if (op_code == NULL)
-		return (NULL);
-
-	return (op_code);
-}
+#include <stdlib.h>
 
 /**
- * main - Entry point for the Monty code interpreter.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- *
- * Return: Returns 0 on success.
- */
+* main - function for monty code interpreter
+* @argc: argument count
+* @argv: argument value
+*
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
 	arg_t arg = {NULL, NULL, NULL, 0};
 
-	char *content, *op;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int linenum = 0;
 
-	validate_arguments(argc, argv);
-	init_args();
-	get_script(argv[1]);
-
-	while (getline(&content, arg.script, &linenum) > 0)
+	if (argc != 2)
 	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	arg.script = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
 		arg.content = content;
-		if (content[0] == '#' || content[0] == '\n')
+		linenum++;
+		if (read_line > 0)
 		{
-			free(content);
-			continue;
+			execute(content, &stack, linenum, file);
 		}
-
-		op = parse_line(content);
-		get_instruction(op, &stack, linenum);
-
 		free(content);
 	}
-
 	free_stack(stack);
-	fclose(arg.script);
-	free_arguments();
-	return (0);
+	fclose(file);
+return (0);
 }
